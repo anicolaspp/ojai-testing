e261cdac2de44a7dd5998f61dc3497d8ee6fb6fb
Nicolas A Perez <anicolaspp@gmail.com>
2019-03-26 22:52:04 -0400

    refactoring

---
+++ src/main/scala/com/mapr/ojai/store/impl/InMemConnection.scala
@@ -0,0 +1,34 @@
+package com.mapr.ojai.store.impl
+
+import java.util
+
+import org.ojai.store._
+import org.ojai.{Document, DocumentBuilder}
+
+class InMemConnection(driver: Driver) extends Connection {
+  override def getValueBuilder: ValueBuilder = driver.getValueBuilder
+
+  override def newDocument(): Document = driver.newDocument()
+
+  override def newDocument(jsonString: String): Document = driver.newDocument(jsonString)
+
+  override def newDocument(map: util.Map[String, AnyRef]): Document = driver.newDocument(map)
+
+  override def newDocument(bean: Any): Document = driver.newDocument(bean)
+
+  override def newDocumentBuilder(): DocumentBuilder = driver.newDocumentBuilder()
+
+  override def newMutation(): DocumentMutation = driver.newMutation()
+
+  override def newCondition(): QueryCondition = driver.newCondition()
+
+  override def newQuery(): Query = driver.newQuery()
+
+  override def newQuery(queryJson: String): Query = driver.newQuery(queryJson)
+
+  override def getDriver: Driver = driver
+
+  override def close(): Unit = {}
+
+  override def getStore(storeName: String): DocumentStore = new InMemoryStore()
+}
--- src/main/scala/com/mapr/ojai/store/impl/InMemDriver.scala
+++ src/main/scala/com/mapr/ojai/store/impl/InMemDriver.scala
@@ -40,30 +40,3 @@ object InMemDriver extends Driver {
   override def connect(url: String, options: Document): Connection = new InMemConnection(this)
 }
 
-class InMemConnection(driver: Driver) extends Connection {
-  override def getValueBuilder: ValueBuilder = driver.getValueBuilder
-
-  override def newDocument(): Document = driver.newDocument()
-
-  override def newDocument(jsonString: String): Document = driver.newDocument(jsonString)
-
-  override def newDocument(map: util.Map[String, AnyRef]): Document = driver.newDocument(map)
-
-  override def newDocument(bean: Any): Document = driver.newDocument(bean)
-
-  override def newDocumentBuilder(): DocumentBuilder = driver.newDocumentBuilder()
-
-  override def newMutation(): DocumentMutation = driver.newMutation()
-
-  override def newCondition(): QueryCondition = driver.newCondition()
-
-  override def newQuery(): Query = driver.newQuery()
-
-  override def newQuery(queryJson: String): Query = driver.newQuery(queryJson)
-
-  override def getDriver: Driver = driver
-
-  override def close(): Unit = {}
-
-  override def getStore(storeName: String): DocumentStore = new InMemoryStore()
-}
